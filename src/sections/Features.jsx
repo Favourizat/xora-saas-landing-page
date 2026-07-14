57e90a20c2b1a7f290453083c4427181c27abbce
Favouriza <114188895+Favourizat@users.noreply.github.com>
2026-07-14 10:26:17 +0100

    Refactor Features component for improved styling
    Updated class names for better responsiveness and styling.

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -11,7 +11,7 @@ const Features = () => {
                     border-2 border-s3 rounded-7xl md:overflow-hidden max-md:flex-col 
                     feature:after md:g7 max-md:border-none max:md-rounded-none max-md:gap-3">
                         {features.map(({id, icon, caption, title, text, button}) => (
-                            <div key={id} className="relative z-2 md:px-10 px-5 md:pb-10 pb-5 flex-50 
+                            <div key={id} className="relative z-2 md:px-8 px-5 md:pb-10 pb-5 flex-50 
                             max-md:g7 max-md:border-2 max-md:border-s3 max-md:rounded-3xl max-md:flex-320">
                                 <div className="w-full flex justify-start items-start">
                                     <div className="-ml-3 mb-12 flex items-center justify-center flex-col">
@@ -22,7 +22,7 @@ const Features = () => {
                                     </div>
                                 </div>
 
-                                <p className="caption mb-5 max-md:mb-6 max-md:h3">{caption }</p>
+                                <p className="caption mb-5 max-md:mb-5 max-md:h2">{caption }</p>
                                 <h2 className="max-w-400 mb-7 h3 text-p4 max-md:mb-6 max-md:h5">{title }</h2>
 
                                 <p className="mb-11 body-1 max-md:mb-8 max-md:body-3">
