b29e3d0366f5bb72b950e5059cc5e63701bd0be8
Favouriza <114188895+Favourizat@users.noreply.github.com>
2026-07-14 11:21:40 +0100

    Adjust padding in testimonial header
    Updated padding for the testimonial header section.

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -7,7 +7,7 @@ const Testimonials = () => {
     return (
             <section className="relative z-2 py-24 md:py-28 lg:py-48">
                 <div className="container block lg:flex">
-                    <div className="testimonial_-head-res relative z-2 mr-2= flex-300 pb-10">
+                    <div className="testimonial_-head-res relative z-2 mr-2= flex-300 pb-15">
                         <p className="caption mb-5 max-md:mb-2.5">Wall of Love</p>
                         <h3 className="h3 max-md:h5 text-p4">Words from our fans</h3>
                     </div>
